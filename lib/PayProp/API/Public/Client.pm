518957471cd7f8f7fd71fec965743dff4b61395e
yangakandeni <kandeni.yanga@gmail.com>
2023-11-02 10:22:44 +0200

    0.02

--- lib/PayProp/API/Public/Client.pm
+++ lib/PayProp/API/Public/Client.pm
@@ -9,7 +9,7 @@ with qw/ PayProp::API::Public::Client::Role::Attribute::Domain /;
 with qw/ PayProp::API::Public::Client::Role::Attribute::Authorization /;
 
 # ABSTRACT: PayProp API Public Client
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 has export => (
 	is => 'ro',
